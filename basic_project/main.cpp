0411cfe21a87ed0b52cbbe18b8cadd2e0c9b8e8e
wen2ms <1819509111@qq.com>
2024-12-02 13:49:52 +0800

    Feat: config file used to define variables

--- basic_project/main.cpp
+++ basic_project/main.cpp
@@ -1,8 +1,12 @@
 #include <cmath>
 #include <iostream>
+#include <string>
+
+#include "basic_project_config.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
+        std::cout << argv[0] << " VERSION " << basic_project_VERSION_MAJOR << '.' << basic_project_VERSION_MINOR << std::endl;
         std::cout << "Usage: " << argv[0] << " number" << std::endl;
         return 1;
     }
