d3a0edd1ca27d16abd2d1b133bff1ef86c6ae4d9
wen2ms <1819509111@qq.com>
2024-12-02 12:44:35 +0800

    Feat: set cxx standard version

--- basic_project/main.cpp
+++ basic_project/main.cpp
@@ -1,5 +1,4 @@
 #include <cmath>
-#include <cstdlib>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -8,7 +7,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    const double input_value = std::atof(argv[1]);
+    const double input_value = std::stod(argv[1]);
     const double output_value = std::sqrt(input_value);
 
     std::cout << "The square roof of " << input_value << " is " << output_value << std::endl;
