68c69a827134ebe10e5d4ac90c88b1719dea96f3
wen2ms <1819509111@qq.com>
2024-12-03 17:44:39 +0800

    Feat: add a option to build

--- add_library/math_functions/math_functions.cpp
+++ add_library/math_functions/math_functions.cpp
@@ -1,9 +1,17 @@
 #include "math_functions.h"
 
+#include <cmath>
+
+#ifdef USE_MYMATH
 #include "mysqrt.h"
+#endif
 
 namespace math_functions {
 double sqrt(double x) {
+#ifdef USE_MYMATH
     return detail::mysqrt(x);
+#else
+    return std::sqrt(x);
+#endif
 }
 }  // namespace math_functions
