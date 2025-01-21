c380889eb3c90f36ea2e67c60734bf392a30eff6
wen2ms <1819509111@qq.com>
2025-01-21 15:04:21 +0800

    Feat: create library

---
+++ create_lib/include/math_functions.h
@@ -0,0 +1,12 @@
+#ifndef MATH_FUNCTIONS_H
+#define MATH_FUNCTIONS_H
+
+float add(float a, float b);
+
+float subtract(float a, float b);
+
+float mutiply(float a, float b);
+
+float divide(float a, float b);
+
+#endif  // MATH_FUNCTIONS_H
---
+++ create_lib/src/math_functions.cpp
@@ -0,0 +1,17 @@
+#include "math_functions.h"
+
+float add(float a, float b) {
+    return a + b;
+}
+
+float subtract(float a, float b) {
+    return a - b;
+}
+
+float mutiply(float a, float b) {
+    return a * b;
+}
+
+float divide(float a, float b) {
+    return a / b;
+}
